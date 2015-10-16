794fb280b3aeb701e045892ec723d94962041a78
João Santos <jmcs@jsantos.eu>
2015-10-16 08:43:43 +0200

    Init command

--- ponto/__main__.py
+++ ponto/__main__.py
@@ -7,15 +7,30 @@ from .scm import GitRepository
 
 cli = click.Group()
 
-BASE_DIR = Path.home() / '.ponto'
+BASE_DIR = Path.home() / '.ponto'  # type: Path
+
 
 # TODO Init to create base structure
 # TODO git-add to clone repository
 
-@cli.command('clone')
-#@click.argument("git_url")
-def clone():
+@cli.command('init')
+def init():
+    info("Creating directory structure")
+    if not BASE_DIR.exists():
+        BASE_DIR.mkdir()
 
+    config_path = BASE_DIR / 'ponto.yaml'
+
+    info("Creating config files")
+    if not config_path.exists():
+        empty_config = {'SCM': set()}
+        with config_path.open('w') as config_file:
+            yaml.safe_dump(empty_config, config_file, default_flow_style=False)
+
+
+@cli.command('clone')
+# @click.argument("git_url")
+def clone():
     # TODO clone git url
 
     config_path = BASE_DIR / 'ponto.yaml'
